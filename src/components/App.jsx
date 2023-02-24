f1dd26e391a021f1d2a35011cea3770fbaec83c2
miciella <201910804@feualabang.edu.ph>
2023-02-24 14:05:46 +0800

    functions for todo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+    const [item, setNewItem] = useState("");
+    const [addedItem, setItems] = useState([]);
+
+    function updateItems(event){
+       let newItem = event.target.value;
+       setNewItem(newItem);
+    }
+
+    function addItem(){
+        setItems((prevState)=> {
+            return [...prevState, item]
+        })
+        //clear the input button for the next item after clicking the add button
+        setNewItem("")
+    }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={updateItems} type="text" value={item}/>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+            { addedItem.map(todoItem => <li>{todoItem}</li>)
+            }
         </ul>
       </div>
     </div>
